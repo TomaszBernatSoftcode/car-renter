7876db0b1481ef3cab474fe5211f1784e5823299
tbernat <tomasz.bernat@stava.pl>
2019-12-08 16:29:21 +0100

    #1 - Creates shared components for vehicle presentation

---
+++ common_static/js/vue/components/shared/vehicle-card.js
@@ -0,0 +1,53 @@
+Vue.component('vehicle-card', {
+    template: html`
+        <v-card
+            class="d-flex"
+            max-width="128"
+        >
+            <v-img
+                class="white--text align-end"
+                height="200px"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+                <v-card-title>Top 10 Australian beaches</v-card-title>
+            </v-img>
+    
+            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+    
+            <v-card-text class="text--primary">
+                <div>Whitehaven Beach</div>
+    
+                <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
+    
+            <v-card-actions>
+                <v-btn
+                    color="orange"
+                    text
+                >
+                    Zarezerwuj
+                </v-btn>
+    
+                <v-btn
+                    color="orange"
+                    text
+                >
+                    Pokaż na mapie
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    `,
+    props: {
+    },
+    data: function() {
+        return {
+        }
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+
+    },
+});
