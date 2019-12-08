5895563c729383661a0a99d2a1df2e3029baf2f3
tbernat <tomasz.bernat@stava.pl>
2019-12-08 16:30:47 +0100

    #1 - Adjusts renter view component to be able to show vehicle cards

--- common_static/js/vue/components/views/renter_panel.js
+++ common_static/js/vue/components/views/renter_panel.js
@@ -4,11 +4,21 @@ Vue.component('renter-panel', {
             class="fill-height"
             fluid
         >
-          
+            <v-row>
+                <!-- Nawigacja menu modeli -->
+                <v-col cols="3">
+                    <vehicle-models-navigation></vehicle-models-navigation>
+                </v-col>
+                
+                <v-col cols="1"></v-col>
+                
+                <!-- Karty i wyszukiwarka -->
+                <v-col cols="8">
+                    <vehicles-cards-container></vehicles-cards-container>
+                </v-col>
+            </v-row>
         </v-container>
     `,
-    props: {
-    },
     data: function() {
         return {
         }
